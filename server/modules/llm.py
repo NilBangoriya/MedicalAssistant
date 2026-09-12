0386224637376b811cc7ed98d150e0c3b82ce73a
Nil Bangoriya <111910045+NilBangoriya@users.noreply.github.com>
2026-09-12 11:01:04 +0530

    change in model name

--- server/modules/llm.py
+++ server/modules/llm.py
@@ -11,7 +11,7 @@ GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 def get_llm_chain(retriever):
     llm = ChatGroq(
         groq_api_key=GROQ_API_KEY,
-        model_name="llama-3.1-8b-instant"
+        model_name="llama-3.3-70b-versatile"
     )
 
     prompt = PromptTemplate(
